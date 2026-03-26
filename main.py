ae91297da663d0fce7192464228a68d7429e6b13
abhishek-paul-d <paulabhishek393@gmail.com>
2026-03-26 18:18:37 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 
 
 if __name__=='__main__':
@@ -15,13 +16,20 @@ if __name__=='__main__':
         logging.info("Initiate the Data Ingestion")
         dataingestionartifact=data_ingestion.intiate_data_ingestion()
         logging.info("Data Initiation Completed")
-        print(dataingestionartifact)
+        print(dataingestionartifact,"\n")
         datavalidationconfig=DataValidationConfig(traningpipelineconfig)
         data_validation=DataValidation(dataingestionartifact,datavalidationconfig)
         logging.info("Initiate Data Validation")
         datavalidationartifact=data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
-        print(datavalidationartifact)
+        print(datavalidationartifact, "\n")
+        datatransformationconfig=DataTransformationConfig(traningpipelineconfig)
+        logging.info("Data Transformation initiated")
+        data_transformation=DataTransformation(datavalidationartifact,datatransformationconfig)
+        datatransformationartifact=data_transformation.initiate_data_transformation()
+        print(datatransformationartifact, "\n")
+        logging.info("Data Transformation Completed")
+
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -15,7 +15,7 @@ def read_yaml_file(file_path: str)-> dict:
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
-def write_yaml_file(file_path: str, content: object, replace: bool = False):
+def write_yaml_file(file_path: str, content: object, replace: bool = False)->None:
     try:
         if replace:
             if os.path.exists(file_path):
@@ -26,3 +26,25 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False):
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
+
+def save_numpy_array_data(file_path: str, array: np.array)->None:
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            np.save(file_obj,array)
+        
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def save_object(file_path: str, obj: object)->None:
+    try:
+        logging.info("Entered save_object method of main_utils class")
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            pickle.dump(obj,file_obj)
+        logging.info("Exited the save_object method of main_utils class")
+
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
